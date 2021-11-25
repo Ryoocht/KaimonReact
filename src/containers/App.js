abc087d3dc41beacbe9eb660a88f4982b456f2a3
Ryochi <ryo1.t.zubu@gmail.com>
2021-11-25 15:21:40 +1000

    First Commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Kaimon</h1>
+        <hr/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
